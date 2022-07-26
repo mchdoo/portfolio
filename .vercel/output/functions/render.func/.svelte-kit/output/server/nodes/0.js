eca0622fff7d717f57e02dcdf2c695de822bb739
mchdoo <pedro@PC-PEDRO.localdomain>
2022-07-26 11:31:45 -0300

    si

--- .vercel/output/functions/render.func/.svelte-kit/output/server/nodes/0.js
+++
@@ -1,7 +0,0 @@
-import * as module from '../entries/fallbacks/layout.svelte.js';
-
-export { module };
-export const index = 0;
-export const file = '_app/immutable/layout.svelte-a6ff5ec8.js';
-export const imports = ["_app/immutable/layout.svelte-a6ff5ec8.js","_app/immutable/chunks/index-d5c04e56.js"];
-export const stylesheets = [];
--- .vercel/output/functions/render.func/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,7 +0,0 @@
-import * as module from '../entries/fallbacks/error.svelte.js';
-
-export { module };
-export const index = 1;
-export const file = '_app/immutable/error.svelte-1e3720a3.js';
-export const imports = ["_app/immutable/error.svelte-1e3720a3.js","_app/immutable/chunks/index-d5c04e56.js"];
-export const stylesheets = [];
